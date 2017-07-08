36b957d639362d271a94ab8f971e303f3b6bb370
Stanley Tan <stanleytan10@gmail.com>
2017-07-08 16:37:26 -0700

    Done with a few parts of the UI

---
+++ Timetable/CreateMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateMenuViewController.swift
+//  Timetable
+//
+//  Created by Stanley Tan on 7/7/17.
+//  Copyright © 2017 Stanley Tan. All rights reserved.
+//
+
+import UIKit
+
+class CreateMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
